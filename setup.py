488982b31692bb56d14bdea3c6622e56c399ac88
Urtzi Odriozola <uodriozola@codesyntax.com>
2024-05-22 15:28:33 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("LICENSE") as f:
 
 setup(
     name="pytbai",
-    version="1.5.4",
+    version="1.5.5",
     description=(
         "pytbai allows to create, manage and send TicketBai invoices to the"
         " Basque tax authorities"
